fd35d33c955df6a661feaffce423151c6f148144
adrianacodau <adriana.codau@student.usv.ro>
2026-04-21 13:20:09 +0300

    Adaugare proiect WPF pentru afisare medicamente

---
+++ NivelUIWPF/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using LibrarieModele;
+using NivelStocareDate;
+
+namespace NivelUIWPF
+{
+    public partial class MainWindow : Window
+    {
+        private readonly IStocareData adminMedicamente;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string radacinaSolutie = Directory
+                .GetParent(AppDomain.CurrentDomain.BaseDirectory)!
+                .Parent!
+                .Parent!
+                .Parent!
+                .Parent!
+                .FullName;
+
+            string caleFisier = Path.Combine(
+                radacinaSolutie,
+                "GestiuneFarmacie",
+                "bin",
+                "Debug",
+                "net8.0",
+                "Medicamente.txt");
+
+            MessageBox.Show(caleFisier);
+
+            adminMedicamente = new AdministrareMedicamenteFisierText(caleFisier);
+        }
+
+        private void btnIncarca_Click(object sender, RoutedEventArgs e)
+        {
+            var medicament = adminMedicamente.GetMedicamente().FirstOrDefault();
+
+            if (medicament == null)
+            {
+                MessageBox.Show("Nu există medicamente în fișier.");
+                return;
+            }
+
+            txtNume.Text = medicament.nume;
+            txtPret.Text = medicament.pret.ToString();
+            txtProducator.Text = medicament.producator;
+            txtCategorie.Text = medicament.categorie.ToString();
+            txtModAdministrare.Text = medicament.modAdministrare.ToString();
+        }
+    }
+}
